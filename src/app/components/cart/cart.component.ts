3c8b2036a6998f5ddb51dc8a3c3cbf3941752b13
muzammilosman <usman.muzammil@gmail.com>
2019-09-29 00:54:11 +0530

    cart component added

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -12,4 +12,18 @@ export class CartComponent implements OnInit {
   ngOnInit() {
   }
 
+  cartItems = [
+    {
+      itemName: "Tropicana",
+      quantity: 5,
+      price: 15
+    },
+    {
+      itemName: "Lays",
+      quantity: 3,
+      price: 20
+    }
+
+  ]
+
 }
